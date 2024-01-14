0ad8dd0e887c13203bdf599dffa6ff7b318da3c4
lufemoal19 <lufemoal19@gmail.com>
2024-01-14 16:11:29 -0600

    fix settings.py

--- digital_resume/digital_resume/settings.py
+++ digital_resume/digital_resume/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'mediafiles')
 ]
 
 STATIC_URL = "/static/"
